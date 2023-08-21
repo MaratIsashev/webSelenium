db8c352bb341df0c586b244259aab667dd02d866
Marat Isashev <isashevmarat@gmail.com>
2023-08-22 02:09:08 +0300

    test added

--- src/test/java/ru/netology/web/webTest1.java
+++ src/test/java/ru/netology/web/webTest1.java
@@ -1,24 +1,27 @@
 package ru.netology.web;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class webTest1 {
-    private WebDriver driver;
-    @BeforeAll
-    public static void setupAll() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        WebDriverManager.chromedriver().setup();
-    }
 
     @Test
     //System.setProperty("webdriver.chrome.driver","driver/chromedriver");
+    void test1(){
+        open("http://localhost:9999/");
+        $("span[data-test-id=name] input").setValue("Иванов-Петров Иван Иванович");
+        $("span[data-test-id=phone] input").setValue("+79991234567");
+        $("[data-test-id=agreement]").click();
+        $("button[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.text(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
 }
